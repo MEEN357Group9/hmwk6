b0d3a4ae205942cf43ea4a78bc57a08aa10e04ce
HAshby <harleyashby@comcast.net>
2017-11-13 14:16:45 -0600

    rr

--- dydxLorenz_butterfly.m
+++ dydxLorenz_butterfly.m
@@ -4,7 +4,7 @@ function dydxLorenz_butterfly(~,y)
 Y1=isnumeric(Y);
 Y2=size(Y);
 if Y1~=1 || Y2(2)~=3
-    eror(' y must be a numeric matrix with 3 columns.')
+    error(' y must be a numeric matrix with 3 columns.')
 sigma=10;
 b=8/3;
 r=160;
