15a9ba9783768255eca8916657a05064f83723b1
HAshby <harleyashby@comcast.net>
2017-11-13 13:59:21 -0600

    1

--- a6fn.m
+++ a6fn.m
@@ -1,6 +1,19 @@
-function f= a6fn(X,Y)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function f=a6fn(X,Y)
+%{
+In this function, the value of the function 5x^2-xy+y^2-y.
+    Inputs:
+        X: X is a scalar numeric value.
+        Y: Y is a scalar numeric value.
+    Output:
+        f: f is the output of the function. 
+%}
+x1=isscalar(X);
+x2=isnumeric(X);
+y1=isscalar(Y);
+y2=isnumeric(Y);
+if x1~=0 || x2~=1 || y1~=0 || y2~=1
+    error('X and Y must be scalar numbers.') %If you get this error, please try again. 
+end 
 x=X;
 y=Y;
 f=5*x^2-x*y+y^2-y;
