09a2879c2fcae9eff981f4b62fecc5ca495d0429
HAshby <harleyashby@comcast.net>
2017-11-14 18:57:56 -0600

    good butterfly

--- Lorenz_butterfly.m
+++ Lorenz_butterfly.m
@@ -13,15 +13,14 @@ if Y1~=1 || Y2(2)~=3
     error(' y must be a numeric matrix with 3 columns.')
 end
 sigma1=10;
-%b=8/3; Not sure why this is here.
+b=8/3; 
 r=160;
 X=y(1);
 Y=y(2);
-%Z=y(3); Not sure why this is here.
-dXdt=-sigma1*X+sigma1*Y;
-dYdt=-X*Y+r*X-Y;
-%dZdt=X*Y-b*Z; Not sure why this is here. 
-dydx=dXdt/dYdt;
-
+Z=y(3); 
+dX=-sigma1*X+sigma1*Y;
+dY=-X*Y+r*X-Y;
+dZ=X*Y-b*Z; 
+dydx=[dX;dY;dZ];
 end
 
